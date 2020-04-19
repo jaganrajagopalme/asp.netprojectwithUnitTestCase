e2d81cc9c65c7ac4fdcd00a2979d367317530b0d
Jagan <Jaganrajagopalme@gmail.com>
2020-04-19 15:43:24 +0530

    Adding dummy label in repo

--- asp.netprojectwithUnitTestCase/Default.aspx.designer.cs
+++ asp.netprojectwithUnitTestCase/Default.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace asp.netprojectwithUnitTestCase
     {
 
         /// <summary>
+        /// lbldummy control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lbldummy;
+
+        /// <summary>
         /// lblfirstnumber control.
         /// </summary>
         /// <remarks>
